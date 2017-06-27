1b892cb87f756d446b5562aa2d587c92baef91e6
livvy <358238964@qq.com>
2017-06-27 15:00:15 +0800

    v1.0.1

--- app/src/main/java/com/example/livvy/verificationcode/MainActivity.java
+++ app/src/main/java/com/example/livvy/verificationcode/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
+  
 import com.example.verificationcode.view.VerificationCodeView;
 
 
